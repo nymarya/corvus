6cb5fb4cb40a11ca8d9cf542ec94cd5422876f1a
Mayra Azevedo <mayradazevedo@gmail.com>
2019-10-08 21:20:48 -0300

    calculate classes probabilities and add comments

--- src/models/models.py
+++ src/models/models.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 class NaiveBayes:
     """ Implementation of a naïve version of the Bayesian belief network.
     The features are assumed to be independent.
@@ -13,11 +15,36 @@ class NaiveBayes:
     # E.g.:{'f1=1 | A':x, 'f1=1 | A': y}
     all_probabilities = {}
 
-    def train(data: list, labels: list, e: int=4) -> float:
+    def _calc_class_probabilities(self, classes: pd.DataFrame) -> None:
+        """Calculate probabilities for each class to occurr.
+
+        Parameters
+        ----------
+        classes: pandas dataframe
+            pandas dataframe contaning the categorized classes
         """
+        labels = classes.idxmax(axis=1).value_counts()
+        n = classes.shape[0]
+        self.class_probabilities = {label: count/n
+                                    for label, count in labels.items()}
+
+    def train(self, data: pd.DataFrame, labels: list, e: int=1):
+        """
+        Parameters
+        ----------
+        data: pandas dataframe
+            dataframe containg all train data
+        labels: list
+            list of classes columns indexes
+        e: int
+            equivalent sample size, used for adapting to very rare classes.
+            Default value is 1.
         """
         # Extract classes
+        classes = data.iloc[:, labels]
         # Calculate classes probabilities
+        self._calc_class_probabilities(classes)
+        print(self.class_probabilities)
         # For each class
         #   Calculate  
         n = 0  # number of examples that match example
